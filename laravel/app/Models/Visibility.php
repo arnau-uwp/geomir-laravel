a0a60f59f321c10aa812da8b87ac29527fd79b33
jesus <jelenu@fp.insjoaquimmir.cat>
2022-11-15 13:53:04 +0100

    CRUD post place

--- laravel/app/Models/Visibility.php
+++ laravel/app/Models/Visibility.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visibility extends Model
 {
+    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
     use HasFactory;
     
     const public = 1;
--- laravel/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ laravel/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,3 +2,6 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-question"></i> Users</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('visibility') }}"><i class="nav-icon la la-question"></i> Visibilities</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('post') }}"><i class="nav-icon la la-question"></i> Posts</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('place') }}"><i class="nav-icon la la-question"></i> Places</a></li>
